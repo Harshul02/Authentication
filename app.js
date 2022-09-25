a3cb73b71274a813a6f8de85484c7019001e48e6
Harshul Jain <harshulgangwal02@gmail.com>
2022-09-25 19:41:36 +0530

    Created Schema and model

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = mongoose.model("User", userSchema);
+
 app.get("/", function(req,res){
     res.render("home");
 });
